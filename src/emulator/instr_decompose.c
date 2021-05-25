b629dd89d5ece2e8e9db02cf776083efccfade43
Adefila, Quham <quham.adefila20@imperial.ac.uk>
2021-05-25 16:45:25 +0000

    change uint32_t to word32  instr_decompose.c

--- src/emulator/instr_decompose.c
+++ src/emulator/instr_decompose.c
@@ -32,8 +32,8 @@ void rotateRight(word32* operand, int amount) {
     *operand = (msb << 31) | (*operand >> 1);
   }
 }
-uint32_t signExtend(u_int32_t number , int noofbits){
-    uint32_t mask = ((uint32_t) pow( 2, noofbits + 1) - 0.5) ;//0.5 accounts for double inconsistency of rounding across different architectures
+word32 signExtend(word32 number , int noofbits){
+    word32 mask = ((word32) pow( 2, noofbits + 1) - 0.5) ;//0.5 accounts for double inconsistency of rounding across different architectures
     mask = mask << (32 - noofbits - 1);
     return number | (number & (1 << (noofbits-1)) ? mask : 0);
 }
