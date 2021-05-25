80c90bbb30fdf1cc6f0ec5ad4c56c30c73852767
Adefila, Quham <quham.adefila20@imperial.ac.uk>
2021-05-25 16:42:47 +0000

    change type def from uint32_t branch.c

--- src/emulator/branch.c
+++ src/emulator/branch.c
@@ -1,16 +1,15 @@
 #include <stdint.h>
 #include <stdlib.h>
-#include <math.h>
 #include <stdio.h>
 #include "em_general.h"
 
 
 void branch(instr instruction, struct State *state){
-    u_int32_t cpsrMask = 0xF0000000;
-    u_int32_t offsetMask = 0x00FFFFFF;
-    u_int32_t flags = getBits(instruction, cpsrMask , 0);
+    word32 cpsrMask = 0xF0000000;
+    word32 offsetMask = 0x00FFFFFF;
+    word32 flags = getBits(instruction, cpsrMask , 0);
     if (flags == state->cpsr){
-        u_int32_t offset = getBits(instruction , offsetMask, 0) << 2;
+        word32 offset = getBits(instruction , offsetMask, 0) << 2;
         offset = signExtend(offset, 26);
         // offset = offset | ((offset & (1<<25)) ? 0xFC000000 : 0); //specific case of sign extend
         state->pc += offset - 8;
