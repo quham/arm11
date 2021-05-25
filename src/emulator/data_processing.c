1b5f6d79851747d02935ad8199ab21af2c33e955
Andrii Verveha <av1620@texel01.doc.ic.ac.uk>
2021-05-25 13:57:04 +0100

    Instruction made as global variable

--- src/emulator/data_processing.c
+++ src/emulator/data_processing.c
@@ -1,13 +1,15 @@
+
 #include <stdlib.h>
 #include <stdint.h>
 #include <stdio.h>
 #include "data_processing.h"
 #define ROTATION_MULTIPLIER
 
-uint32_t* instruction;
+uint32_t instruction;
 
-void data_processing(uint32_t instruction) {
-  uint32_t opcode = getOpcode(instruction);
+void data_processing(uint32_t new_instruction) {
+  instruction = new_instruction;
+  //  uint32_t opcode = getOpcode(instruction);
   /*  uint32_t operdan2;
   if (checkImmediate(instruction)) {
     operand2 = operandImmediate(instruction);
@@ -16,11 +18,12 @@ void data_processing(uint32_t instruction) {
 
 int main(void) {
   uint32_t instruction = 0b00000010101101010101000000000000;
-  uint32_t s = checkSet(instruction);
-  uint32_t i = checkImmediate(instruction);
-  uint32_t opcode = getOpcode(instruction);
-  uint32_t rn = getRn(instruction);
-  uint32_t rd = getRd(instruction);
+  data_processing(instruction);
+  uint32_t s = checkSet();
+  uint32_t i = checkImmediate();
+  uint32_t opcode = getOpcode();
+  uint32_t rn = getRn();
+  uint32_t rd = getRd();
   
   printf("Instruction is: ");
   printBits(instruction);
@@ -53,60 +56,60 @@ void rotateRight(uint32_t* operand, int amount) {
   }
 }
 
- uint32_t getBits(uint32_t instruction, uint32_t mask, int shiftNo) {
-   uint32_t bits = instruction & mask;
-   return bits >> shiftNo;
- }
+uint32_t getBits(uint32_t instruction, uint32_t mask, int shiftNo) {
+  uint32_t bits = instruction & mask;
+  return bits >> shiftNo;
+}
 
- uint32_t checkImmediate(uint32_t instruction) {
+uint32_t checkImmediate(void) {
   uint32_t mask = 0x2000000;
   return getBits(instruction, mask, 25);
 }
 
-uint32_t checkSet(uint32_t instruction) {
+uint32_t checkSet(void) {
   uint32_t mask = 0x100000;
   return getBits(instruction, mask, 20);
 }
 
-uint32_t getOpcode(uint32_t instruction) {
+uint32_t getOpcode(void) {
   uint32_t mask = 0x1e00000;
   return getBits(instruction, mask, 21);
 }
 
-uint32_t getRn(uint32_t instruction) {
+uint32_t getRn(void) {
   uint32_t mask = 0xf0000;
   return getBits(instruction, mask, 16);
 }
 
-uint32_t getRd(uint32_t instruction) {
+uint32_t getRd(void) {
   uint32_t mask = 0xf000;
   return getBits(instruction, mask, 12);
 }
 
-uint32_t operandRotate(uint32_t instruction) {
+uint32_t operandRotate(void) {
   uint32_t mask = 0xf00;
   return getBits(instruction, mask, 8);
 }
 
-uint32_t operandImmediate(uint32_t instruction) {
+uint32_t operandImmediate(void) {
   uint32_t mask = 0xff;
   return getBits(instruction, mask, 0);
 }
 
-uint32_t operandShift(uint32_t instruction) {
+uint32_t operandShift(void) {
   uint32_t mask = 0xff0;
   return getBits(instruction, mask, 4);
 }
 
-uint32_t operandRm(uint32_t instruction) {
+uint32_t operandRm(void) {
   uint32_t mask = 0xf;
   return getBits(instruction, mask, 0);
 }
 
-uint32_t getOperand(uint32_t instruction) {
+uint32_t getOperand(void) {
   uint32_t operand;
   int rotation;
-  if (checkImmediate(instruction)) {
+  if (checkImmediate()) {
     // operand = operandImmediate(instruction);
     // rotation =  operandRotate(instruction);
     // rotateRight(&operand, ROTATION_MULTIPLIER * rotation);
--- src/emulator/data_processing.h
+++ src/emulator/data_processing.h
@@ -1,14 +1,16 @@
+
+
 void data_processing(uint32_t instruction);
-uint32_t checkImmediate(uint32_t instruction);
-uint32_t checkSet(uint32_t instruction);
-uint32_t getOpcode(uint32_t instruction);
-uint32_t getRn(uint32_t instruction);
-uint32_t getRd(uint32_t instruction);
+uint32_t checkImmediate(void);
+uint32_t checkSet(void);
+uint32_t getOpcode(void);
+uint32_t getRn(void);
+uint32_t getRd(void);
 uint32_t getBits(uint32_t instruction, uint32_t mask, int shiftNo);
-uint32_t operandRotate(uint32_t instruction);
-uint32_t operandImmediate(uint32_t insruction);
-uint32_t oprandShift(uint32_t instruction);
-uint32_t operandRm(uint32_t instruction);
+uint32_t operandRotate(void);
+uint32_t operandImmediate(void);
+uint32_t oprandShift(void);
+uint32_t operandRm(void);
 void rotateRight(uint32_t* operand, int amount);
 
 void printBits(uint32_t instruction);
