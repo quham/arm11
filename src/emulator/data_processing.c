bdd9b874d01409c16a07cc3a2f49842885499893
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2021-05-27 13:31:42 +0100

    uncommented data_processing code

--- src/emulator/data_processing.c
+++ src/emulator/data_processing.c
@@ -19,112 +19,112 @@ void data_processing(word32 new_instruction, State *new_state) {
 // }
 
 // Calculates the value of the operand2.
-// word32 getOperand(void) {
-//   word32 operand;
+word32 getOperand(void) {
+  word32 operand;
 
-//   if (checkImmediate(instruction)) {
-//     operand = operandImmediate();
-//     rotateRight(&operand, ROTATION_MULTIPLIER * operandRotate());
-//   } else {
-//     operand = state->regs[getRm(instruction)];
-//     word32 shift = getBits(instruction, 4, 11);
-//     word32 bit4 = checkBit(shift, 0);
-//     word32 shift_type = getBits(shift, 5, 6);
-//     word32 shift_value;
-//     if (bit4) {
-//       shift_value = state->regs[getRs(instruction)];
-//     } else {
-//       shift_value = getBits(instruction, 7, 11);
-//     }
-//     makeShift(&operand, shift_value, shift_type);
-//   }
-//   return operand;
-// }
+  if (checkImmediate(instruction)) {
+    // operand = operandImmediate();
+    // rotateRight(&operand, ROTATION_MULTIPLIER * operandRotate());
+  } else {
+    operand = state->regs[getRm(instruction)];
+    word32 shift = getBits(instruction, 4, 11);
+    word32 bit4 = checkBit(shift, 0);
+    word32 shift_type = getBits(shift, 5, 6);
+    word32 shift_value;
+    if (bit4) {
+      shift_value = state->regs[getRs(instruction)];
+    } else {
+      shift_value = getBits(instruction, 7, 11);
+    }
+    makeShift(&operand, shift_value, shift_type);
+  }
+  return operand;
+}
 
-// // makes a shift of the operand2 depending on its shift_type
-// void makeShift(word32 *operand, word32 shift_value, word32 shift_type) {
-//   // In case of register provided, selects its first byte.
-//   shift_value = shift_value & 0xff;
-//   //  char carry_out = checkBit(*operand, shift_value - 1);
+// makes a shift of the operand2 depending on its shift_type
+void makeShift(word32 *operand, word32 shift_value, word32 shift_type) {
+  // In case of register provided, selects its first byte.
+  shift_value = shift_value & 0xff;
+  //  char carry_out = checkBit(*operand, shift_value - 1);
 
-//   switch (shift_type) {
-//     case 0:  // logic shift left
-//       //      carry_out = checkBit(*operand, WORD_SIZE - (int) shift_value);
-//       *operand <<= shift_value;
-//       break;
-//     case 1:  // logic shift right
-//       *operand >>= shift_value;
-//       break;
-//     case 2:  // arithmetic shift right
-//       *operand = signExtend(*operand, shift_value);
-//       break;
-//     case 3:  // rotate right
-//       rotateRight(operand, shift_value);
-//       break;
-//   }
+  switch (shift_type) {
+    case 0:  // logic shift left
+      //      carry_out = checkBit(*operand, WORD_SIZE - (int) shift_value);
+      *operand <<= shift_value;
+      break;
+    case 1:  // logic shift right
+      *operand >>= shift_value;
+      break;
+    case 2:  // arithmetic shift right
+      *operand = signExtend(*operand, shift_value);
+      break;
+    case 3:  // rotate right
+      rotateRight(operand, shift_value);
+      break;
+  }
 
-//   if (checkSet(instruction)) {
-//     // *(state->regs + CPSR_INDEX) = setBitC(carry_out);
-//   }
+  if (checkSet(instruction)) {
+    // *(state->regs + CPSR_INDEX) = setBitC(carry_out);
+  }
 
-//   return;
-// }
+  return;
+}
 
-// void performOperation(void) {
-//   word32 result;
-//   word32 opcode = getBits(instruction, 21, 24);
-//   word32 operand2 = getOperand();
-//   word32 rn = state->regs[getBits(instruction, 15, 19)];  // can we use getRn here?
-//   word32 *rd = state->regs + getBits(instruction, 16, 19);
-//   //  word32 carry_out = checkBit(state->regs[CPSR_INDEX], 31);
+void performOperation(void) {
+  word32 result;
+  word32 opcode = getBits(instruction, 21, 24);
+  word32 operand2 = getOperand();
+  word32 rn = state->regs[getBits(instruction, 15, 19)];  // can we use getRn here?
+  word32 *rd = state->regs + getBits(instruction, 16, 19);
+  //  word32 carry_out = checkBit(state->regs[CPSR_INDEX], 31);
 
-//   switch (opcode) {
-//     case 0:  // 0000 and
-//       result = rn & operand2;
-//       *rd = result;
-//       break;
-//     case 1:  // 0001 eor
-//       result = rn ^ operand2;
-//       *rd = result;
-//       break;
-//     case 2:  // 0010 sub
-//       result = rn - operand2;
-//       *rd = result;
-//       break;
-//     case 3:  // 0011 rsb
-//       result = operand2 - rn;
-//       *rd = result;
-//       break;
-//     case 4:  // 0100 add
-//       result = rn + operand2;
-//       *rd = result;
-//       break;
-//     case 8:  // 1000 tst
-//       result = rn & operand2;
-//       break;
-//     case 9:  // 1001 teq
-//       result = rn ^ operand2;
-//       break;
-//     case 10:  // 1010 cmp
-//       result = rn - operand2;
-//       break;
-//     case 12:  // 1100 orr
-//       result = rn | operand2;
-//       *rd = result;
-//       break;
-//     case 13:  // 1101 mov
-//       result = operand2;
-//       *rd = result;
-//       break;
-//   }
+  switch (opcode) {
+    case 0:  // 0000 and
+      result = rn & operand2;
+      *rd = result;
+      break;
+    case 1:  // 0001 eor
+      result = rn ^ operand2;
+      *rd = result;
+      break;
+    case 2:  // 0010 sub
+      result = rn - operand2;
+      *rd = result;
+      break;
+    case 3:  // 0011 rsb
+      result = operand2 - rn;
+      *rd = result;
+      break;
+    case 4:  // 0100 add
+      result = rn + operand2;
+      *rd = result;
+      break;
+    case 8:  // 1000 tst
+      result = rn & operand2;
+      break;
+    case 9:  // 1001 teq
+      result = rn ^ operand2;
+      break;
+    case 10:  // 1010 cmp
+      result = rn - operand2;
+      break;
+    case 12:  // 1100 orr
+      result = rn | operand2;
+      *rd = result;
+      break;
+    case 13:  // 1101 mov
+      result = operand2;
+      *rd = result;
+      break;
+  }
 
-//   /*
-//   if (checkSet(instruction)) {
-//     char Z = (result == 0) ? 1 : 0;
-//     char N = checkBit(result, 31);
-//     check for overflow here (or in the add related cases)
+  /*
+  if (checkSet(instruction)) {
+    char Z = (result == 0) ? 1 : 0;
+    char N = checkBit(result, 31);
+    check for overflow here (or in the add related cases)
 
-//     May look up conditions in comp arch;
-//   */
-//   return;
-// }
+    May look up conditions in comp arch;
+  */
+  return;
+}
