9549b7609770dd10bf3dd555e1e5cda1e1d67ec2
Adefila, Quham <quham.adefila20@imperial.ac.uk>
2021-05-27 15:48:31 +0000

    Update branch.c

--- src/emulator/branch.c
+++ src/emulator/branch.c
@@ -5,10 +5,8 @@
 #include "em_general.h"
 
 void branch(instr instruction, State *state) {
-  if (checkCond(instruction, state)) {
-    word32 offset = getBits(instruction, 0, 23) << 2;  // offset bits shifted ny 2
-    offset = signExtend(offset, 26);
-    // offset = offset | ((offset & (1<<25)) ? 0xFC000000 : 0); // specific case of signExtend
-    state->regs[PC_INDEX] += offset - 8;
-  }
+  word32 offset = getBits(instruction, 0, 24) << 2;  // offset bits shifted ny 2
+  offset = signExtend(offset, 26);
+  // offset = offset | ((offset & (1<<25)) ? 0xFC000000 : 0); // specific case of signExtend
+  state->regs[PC_INDEX] += offset - 8;
 }
