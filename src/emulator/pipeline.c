a38c39bfef069e260de39ec5c0d551cf3795786f
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2021-05-29 02:08:30 +0100

    aligned register print

--- src/emulator/pipeline.c
+++ src/emulator/pipeline.c
@@ -9,7 +9,7 @@ void printState(State* state) {
 
   printf("Registers:\n");
   for (int i = 0; i <= 12; i++) {  // iterate numbered registers
-    printf("$%3d:%10d (0x%08x) \n", i, regs[i], regs[i]);
+    printf("$%-3d:%10d (0x%08x) \n", i, regs[i], regs[i]);
   }
   printf("PC  :%10d (0x%08x) \n", regs[PC_INDEX], regs[PC_INDEX]);
   printf("CSPR:%10d (0x%08x) \n", regs[CPSR_INDEX], regs[CPSR_INDEX]);
@@ -17,7 +17,7 @@ void printState(State* state) {
   for (int i = 0; i < MEMORY_SIZE / 4; i++) {
     word32 chunk = fetch(4 * i, state);
     if (chunk != 0) {
-      printf("%08lx: 0x%08x\n", sizeof(i) * i, chunk);
+      printf("0x%08lx: 0x%08x\n", sizeof(i) * i, chunk);
     }
   }
 }
