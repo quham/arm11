d5e529b9932f8750e1bcba51aae020345eb89d58
Oliver Madine <30090176+OliverMadine@users.noreply.github.com>
2021-06-08 19:39:16 +0100

    updated make clean to remove executables

--- src/assembler/tokenizer.c
+++ src/assembler/tokenizer.c
@@ -9,7 +9,7 @@ tokenset tokenize(char line[]) {
 
   if (line[0] != '\0') {
     char *instruction = line;
-    // strcpy(tokens.opcode, strtok_r(instruction, " ", &instruction));
+    strcpy(tokens.opcode, strtok_r(instruction, " ", &instruction));
 
     char *reg = strtok(instruction, ",");
     int op = 0;
