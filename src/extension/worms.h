6f7af459c9b14899ae72eee2868fc5b4da972ffc
Nicholas Fry <nf20@ic.ac.uk>
2021-06-09 15:34:26 +0200

    refactor: extension-inital file structure, docs: extension Makefile

---
+++ src/extension/worms.h
@@ -0,0 +1,9 @@
+#ifndef WORMSCII_H
+#define WORMSCII_H
+
+
+/* TO BE CONFIRMED */
+#define MAP_WIDTH 64  
+#define MAP_HEIGHT 128
+
+#endif
---
+++ src/extension/wormscii.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(void) {
+
+
+    printf("                                    \n");
+    printf("      ~^~@  WELCOME TO WORMSCII  @~^~\n");
+    printf("\n");
+    printf(" *~       HELP THE WORLD RECOVER  ~*     \n");
+    printf("   ~*  EXTERMINATE MISBEHAVING WORMS  *~ \n\n");
+
+
+    //INTRODUCTION / GAME RULES ??
+
+
+    /* ENTER GAME LOOP */
+
+}
