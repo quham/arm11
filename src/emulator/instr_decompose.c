8dec9155d908a3ff983972fdbcfc7b046c154b4e
Adefila, Quham <quham.adefila20@imperial.ac.uk>
2021-06-09 11:18:47 +0000

    remove getbits

--- src/emulator/instr_decompose.c
+++ src/emulator/instr_decompose.c
@@ -23,11 +23,6 @@ bool checkSet(instr instruction) {
   return checkBit(instruction, 20);
 }
 
-// inclusive start_index, exclusive end_index, indices start from LSB to MSB
-word32 getBits(word32 word, int start_index, int end_index) {
-  word32 mask = UINT32_MAX >> (WORD_SIZE - (end_index - start_index));
-  return (word >> start_index) & mask;
-}
 
 // index starts from LSB
 byte getByte(word32 word, int index) {
