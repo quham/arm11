9d02e37a0a5fd10eb71ba770c751fcd1fb87a8ad
Nicholas Fry <nf20@ic.ac.uk>
2021-06-11 21:23:07 +0200

    feat: added a time variable to base the coordinates on

--- src/extension/parabola.c
+++ src/extension/parabola.c
@@ -7,28 +7,49 @@
 #define PI  3.14159
 #define GRAVITY 9.8
 
-double getY(double x, double angle, double u) {
-  return ((x * tan(angle)) - ((GRAVITY * pow(x, 2) * (1 + pow(tan(angle), 2)) / (2 * pow(u, 2)))));
+bool inMap(coordinate position) {
+  return position.x >= 0 && position.y >= 0 && position.x <= MAP_WIDTH;
 }
 
-player_input toRadians(player_input input) {
-  input.angle = (input.angle * PI) / 180;
-  return input;
+void printCoordinates(coordinate *coords) {
+  while (inMap(coords[0])) {
+    printf("(%d,%d)\n", (int)coords->x, (int)coords->y); 
+    coords++;
+  }
 }
 
+double getY(double initial_velocity, double angle, double x) {
+  return ((x * tan(angle)) - ((GRAVITY * pow(x, 2) * (1 + pow(tan(angle), 2)) / (2 * pow(initial_velocity, 2)))));
+}
+
+double getX(double initial_veloctiy, double angle, double time) {
+  return (initial_veloctiy * time * cos(angle));
+}
+
+void toRadians(player_input *input) {
+  input->angle = (input->angle * PI) / 180;
+}
 
 coordinate *parabola(player_input input) {
-  input = toRadians(input);
+  toRadians(&input);
+ 
+  coordinate *coords = malloc(sizeof(coordinate) * MAP_HEIGHT * MAP_WIDTH); //TODO: maxsize 
+  coordinate position = {0, MAP_HEIGHT}; //TODO: turret pos - will be given
 
-  coordinate *coords = malloc(sizeof(coordinate) * 128 * 2); //TODO: maxsize 
-  coordinate position = {0, 0};
-  bool is_moving = false;
-  while (position.y > 0 || !is_moving) {
-    position.x++; //TODO: change to use time! 
-    position.y = getY(position.x, input.angle, input.power);
-
-    coords[(int)position.x] = position;
-    is_moving = true;
+  int coord = 0;
+  for (double time = 0; inMap(position); time+=0.1) {
+    position.x = getX(input.power, input.angle, time);
+    position.y = MAP_HEIGHT - getY(input.power, input.angle, position.x);
+    coords[coord] = position;
+    coord++;
   }
+
   return coords;
+}
+
+void printParabola(char map[MAP_HEIGHT][MAP_WIDTH], coordinate points[]) {
+    for (int i = 0; i < 50; i++) {
+        map[(int) points[i].y][(int) points[i].x] = '.';
+    }
+    printMap(map);
 }
